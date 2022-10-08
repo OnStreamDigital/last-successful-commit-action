0e257bc7721a006895060ed0af62b543e1a15fcb
Simon M <simondotm@users.noreply.github.com>
2022-10-08 21:16:06 +0100

    console.log action outputs

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const github = require("@actions/github");
 try {
   const octokit = github.getOctokit(core.getInput("github_token"));
   const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
+  const branch = core.getInput("branch")
+  const workflow_id = core.getInput("workflow_id")
   const params = {
     owner,
     repo,
-    workflow_id: core.getInput("workflow_id"),
+    workflow_id: workflow_id,
     status: "success",
-    branch: core.getInput("branch"),
+    branch: branch,
   }
   // optionally filter workflow runs by the event that triggered them
   // unlike v1, there is no longer a default event of "push", the default event type is "any"
@@ -24,7 +26,11 @@ try {
         res.data.workflow_runs.length > 0
           ? res.data.workflow_runs[0].head_commit.id
           : "";
+
+      console.log(`found ${res.data.workflow_runs.length} runs of workflow '${workflow_id}' for branch '${branch}', commit hash is '${lastSuccessCommitHash}'`)
+  
       core.setOutput("commit_hash", lastSuccessCommitHash);
+
     })
     .catch((e) => {
       core.setFailed(e.message);
